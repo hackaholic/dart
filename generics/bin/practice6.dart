f14e67dfc7950235c79131282239e33dc1e44edd
hackaholic <juniourkawasakicool@gmail.com>
2024-02-01 17:55:47 +0530

    generics

--- generics/bin/practice6.dart
+++ generics/bin/practice6.dart
@@ -1,7 +1,7 @@
 T merge<T, R>(T item1, T item2) {
   if (item1 is num && item2 is num) {
     return item1 + item2 as T;
-  } else if (item1 is num && item2 is String) {
+  } else if (item1 is num || item2 is num) {
     return '$item1 $item2' as T;
   } else {
     return '$item1 $item2' as T;
@@ -9,7 +9,7 @@ T merge<T, R>(T item1, T item2) {
 }
 
 void main(List<String> args) {
-  print(merge(1, 2));
+  print(merge<int, int>(1, 2));
   print(merge(3.14, 5.6));
   print(merge('Hello', 2));
   print(merge('coco', 'Coco'));
